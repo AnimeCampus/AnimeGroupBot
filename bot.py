f7f0ef3668b2cdab137564faefe3c3746857d5f4
Madara Uchiha <141549063+MadaraUchihaBot@users.noreply.github.com>
2023-08-06 23:45:57 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,7 @@ async def welcome(_, message):
             image_height = 720
             
             # Load the custom welcome template image
-            welcome_image = Image.open("template/UchihaMadara.png")
+            welcome_image = Image.open("madara uchiha.jpg")
             welcome_image = welcome_image.resize((image_width, image_height))
             
             # Load and resize the new user's profile picture
